240f9be11b4a647eadf93598fc8f5c1d9ca45dd6
mrmcree <363248292@qq.com>
2019-03-13 10:03:44 +0800

    'dd'

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ module.exports = {
     email: {
         service: "QQ",
         user: '363248292@qq.com',
-        pass: 'corhdzzwznjxbjcf'
+        pass: '*****'
     },
     apiUrl:'https://weatherapi.market.xiaomi.com/wtr-v3/weather/all?latitude=110&longitude=112&isLocated=true&locationKey=weathercn%3A101020200&days=15&appKey=weather20151024&sign=zUFJoAR2ZVrDy1vF3D07&romVersion=7.2.16&appVersion=87&alpha=false&isGlobal=false&device=cancro&modDevice=&locale=zh_cn'
 }
